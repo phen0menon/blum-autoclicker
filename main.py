1925f69fc8535ad505cba6fc79b7921d0cc50693
phen0menon <roland.ibr@yahoo.com>
2024-07-18 22:40:51 +0200

    feat: add locales

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import logging
 import random
 import time
-from typing import Any, List, Optional, TypedDict
+from typing import Any, Dict, List, Literal, Optional, TypedDict
 
 import cv2
 import keyboard
@@ -11,7 +11,31 @@ import pygetwindow as gw
 from mss import mss
 from ultralytics import YOLO
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
+
+Language = Literal["en", "ru"]
+Translations = Dict[str, str]
+
+locales: Dict[Language, Translations] = {
+    "en": {
+        "loading": "Loading pretrained model...",
+        "window_not_found": "Window not found, did you open BLUM app?",
+        "error_getting_window": "Error getting window",
+        "ready": "Ready: Start the Drop Game and then press 'k' to start, 'l' to stop",
+        "cold_start": "Model is running first time, may take a few seconds...",
+        "stopped": "⏸️  Stopped, press 'k' to start again",
+        "running": "🔥 Running, press 'l' to stop",
+    },
+    "ru": {
+        "loading": "Загрузка обученной модели...",
+        "window_not_found": "Окно не найдено, вы открыли приложение BLUM?",
+        "error_getting_window": "Ошибка получения окна",
+        "ready": "Готово: Запустите мини-игру и нажмите 'k' для начала, 'l' для остановки",
+        "cold_start": "Модель запускается впервые, это может занять несколько секунд...",
+        "stopped": "⏸️  Остановлено, для запуска снова нажмите 'k'",
+        "running": "🔥 Запущено, для остановки нажмите 'l'",
+    },
+}
 
 
 class Window(TypedDict):
@@ -25,12 +49,12 @@ def get_point_center(x1: int, y1: int, x2: int, y2: int) -> float:
     return (x1 + x2) / 2, (y1 + y2) / 2
 
 
-def get_window() -> Optional[Window]:
+def get_window(locale: Translations) -> Optional[Window]:
     try:
         windows = gw.getWindowsWithTitle("TelegramDesktop")
 
         if not windows:
-            raise Exception("Window not found, did you open BLUM app?")
+            raise Exception(locale["window_not_found"])
 
         window = windows[0]
 
@@ -43,16 +67,18 @@ def get_window() -> Optional[Window]:
             "top": window.top,
             "width": window.width,
         }
-    except Exception as ee:
-        logging.error("Error getting window: %s", ee)
+    except Exception as exc:
+        logging.error("%s: %s", locale["error_getting_window"], exc)
         window = None
 
 
 class Runner:
-    def __init__(self):
+    def __init__(self, locale: Translations):
         self.cancelled = True
         self.clicks = 0
         self.init_keybindings()
+        self.locale = locale
+        self.cold_start = True
 
     def init_keybindings(self):
         keyboard.on_press(self.handle_keyboard_press)
@@ -60,7 +86,9 @@ class Runner:
     def handle_keyboard_press(self, event):
         if event.name == "l":
             self.cancelled = True
+            logging.info(self.locale["stopped"])
         elif event.name == "k":
+            logging.info(self.locale["running"])
             self.cancelled = False
 
     def detect_figure_and_click(self, detected: List[Any], window: Window) -> None:
@@ -73,7 +101,7 @@ class Runner:
             for box in result.boxes:
                 x1, y1, x2, y2 = box.xyxy[0]
                 title = box.cls.item()
-                # print(f"[title] - {box.cls.item()} [conf] - {box.conf.item()}")
+                # print(f"{box.cls.item()}; {box.conf.item()}")
 
                 # ignore bombs
                 if title == 3:
@@ -135,17 +163,16 @@ class Runner:
         return False
 
     def run(self):
-        logging.info("Loading pretrained model...")
+        logging.info(self.locale["loading"])
         self.model = YOLO("best.pt")
-        logging.info("Model loaded")
-        logging.info("Ready: Press 'k' to start, 'l' to stop")
+        logging.info(self.locale["ready"])
 
         while True:
             if self.cancelled:
                 time.sleep(0.1)
                 continue
 
-            window = get_window()
+            window = get_window(self.locale)
 
             if not window:
                 time.sleep(1)
@@ -153,12 +180,16 @@ class Runner:
 
             try:
                 screenshot = self.grab_screenshot(window)
-                
+
                 # autoreplay feature
                 if self.find_replay_button(screenshot, window):
                     continue
 
-                detected: List[Any] = self.model(screenshot)
+                if self.cold_start:
+                    logging.info(self.locale["cold_start"])
+                    self.cold_start = False
+
+                detected: List[Any] = self.model(screenshot, verbose=False)
                 self.detect_figure_and_click(detected, window)
                 time.sleep(0.006)
             except Exception as e:
@@ -166,6 +197,23 @@ class Runner:
                 continue
 
 
+def get_user_language():
+    while True:
+        logging.info(
+            "Choose your language (выберите ваш язык):\n\t1 - English, 2 - Русский"
+        )
+
+        language_id = int(input())
+
+        if language_id == 1:
+            return "en"
+        elif language_id == 2:
+            return "ru"
+
+
 if __name__ == "__main__":
-    runner = Runner()
+    lang: Language = get_user_language()
+    locale: Translations = locales[lang]
+
+    runner = Runner(locale)
     runner.run()
